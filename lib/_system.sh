2408dd6b95422e138daa434de246ec2b23be1573
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2025-05-21 10:11:33 -0300

    Update _system.sh
    Signed-off-by: Lucas Saud <90074068+LucasSaud@users.noreply.github.com>

--- lib/_system.sh
+++ lib/_system.sh
@@ -359,7 +359,7 @@ system_git_clone() {
   sudo chown -R deploy:deploy /home/deploy/empresa
   
   # Tentar clonar o repositório
-  if sudo -u deploy git clone https://lucassaud:${token_code}@github.com/AutoAtende/Sys3.git /home/deploy/empresa/ ; then
+  if sudo -u deploy git clone -b eire5 https://lucassaud:${token_code}@github.com/AutoAtende/Sys3.git /home/deploy/empresa/ ; then
     printf "\n${GREEN} ✅ Repositório clonado com sucesso!${GRAY_LIGHT}"
   else
     printf "\n${RED} ⚠️ Falha ao clonar o repositório. Verificando conectividade...${GRAY_LIGHT}"
